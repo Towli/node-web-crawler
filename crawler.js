7a036e33fd7f8595bd823a8c283a919476847a52
Alex Towli <alextowli@hotmail.co.uk>
2017-03-06 01:28:12 +0000

    initial commenting of crawler functions

--- crawler.js
+++ crawler.js
@@ -3,6 +3,11 @@ var jquery = require('jquery');
 var cheerio = require('cheerio');
 var url = require('url');
 
+/**
+ * Constructor for a Crawler object. Initialises the core components used to model 
+ * queues for visiting pages in sequence, storage for asset scraping
+ * @param  {base_url}	The base (starting) url of crawler
+ */
 function Crawler(base_url) {
     if (arguments.length > 1 || arguments.length < 1) {
         throw new Error("WebCrawler takes a single URL when initialized.");
@@ -17,8 +22,12 @@ function Crawler(base_url) {
 	Crawler.static_assets = [];
 }
 
+/**
+ * Stores the first page to visit onto the queue ready for crawling, and
+ * calls the crawl() function to begin the crawling process
+ * @param  {callback}
+ */
 Crawler.prototype.start = function(callback) {
-	console.log(url.parse(Crawler.base_url).href);
 	Crawler.pages_to_visit.push(url.parse(Crawler.base_url).href);
 	console.log("Crawler first page pushed: " + Crawler.pages_to_visit);
 	Crawler.prototype.crawl(function() {
@@ -27,6 +36,13 @@ Crawler.prototype.start = function(callback) {
 	});
 }
 
+/**
+ * Begins the crawling process by popping the next page to visit from the queue
+ * and determining whether it is crawlable and/or whether it has already been
+ * visited. This function will execute recursively until the base case is met
+ * Base case: (next_page === undefined)
+ * @param  {callback}
+ */
 Crawler.prototype.crawl = function(callback) {
 	var next_page = Crawler.pages_to_visit.pop();
 	if (next_page == undefined) {
@@ -48,6 +64,13 @@ Crawler.prototype.crawl = function(callback) {
 	}
 }
 
+/**
+ * Sends a request to the given URL, loads and then stores the DOM locally.
+ * The DOM object is passed to collect_internal_links() and scrape_static_assets()
+ * functions for the scraping phases of the crawl
+ * @param  {url} 
+ * @param  {callback}
+ */
 Crawler.prototype.visit_page = function(url, callback) {
 	Crawler.pages_visited[url] = true;	// Add visit page to the set
 	console.log("Added " + url + " to pages_visited.");
@@ -73,6 +96,11 @@ Crawler.prototype.visit_page = function(url, callback) {
 	});
 }
 
+/**
+ * Scrapes and stores all internal links of the current DOM body and stores
+ * the relative links into the Crawler's queue of pages to visit
+ * @param  {$}	
+ */
 Crawler.prototype.collect_internal_links = function($) {
 	var all_relative_links = [];
 	var all_absolute_links = [];
@@ -95,6 +123,13 @@ Crawler.prototype.collect_internal_links = function($) {
 	});
 }
 
+/**
+ * Scrapes all static assets from the current DOM body by use of jQuery to scope
+ * and select scripts, links, and images from their respective html tags
+ * @param  {url}
+ * @param  {$}
+ * @param  {callback}
+ */
 Crawler.prototype.scrape_static_assets = function(url, $, callback) {
 	var $head = $('head');
 	var $body = $('body');
