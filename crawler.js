40a7a20415eb3320c26ae9b69cf0559b4eabb262
Alex Towli <alextowli@hotmail.co.uk>
2017-03-06 01:00:58 +0000

    refactor: rename cheerio-crawler to crawler

--- crawler.js
+++ crawler.js
@@ -3,53 +3,53 @@ var jquery = require('jquery');
 var cheerio = require('cheerio');
 var url = require('url');
 
-function CheerioCrawler(base_url) {
+function Crawler(base_url) {
     if (arguments.length > 1 || arguments.length < 1) {
         throw new Error("WebCrawler takes a single URL when initialized.");
     }
     if (typeof base_url !== "string") {
         throw new Error("WebCrawler needs a URL string for the base url.");
     }
-	CheerioCrawler.base_url = base_url;
-	CheerioCrawler.pages_visited = {};
-	CheerioCrawler.pages_to_visit = [];
-	CheerioCrawler.crawl_counter = 0;
-	CheerioCrawler.static_assets = [];
+	Crawler.base_url = base_url;
+	Crawler.pages_visited = {};
+	Crawler.pages_to_visit = [];
+	Crawler.crawl_counter = 0;
+	Crawler.static_assets = [];
 }
 
-CheerioCrawler.prototype.start = function(callback) {
-	console.log(url.parse(CheerioCrawler.base_url).href);
-	CheerioCrawler.pages_to_visit.push(url.parse(CheerioCrawler.base_url).href);
-	console.log("CheerioCrawler first page pushed: " + CheerioCrawler.pages_to_visit);
-	CheerioCrawler.prototype.crawl(function() {
+Crawler.prototype.start = function(callback) {
+	console.log(url.parse(Crawler.base_url).href);
+	Crawler.pages_to_visit.push(url.parse(Crawler.base_url).href);
+	console.log("Crawler first page pushed: " + Crawler.pages_to_visit);
+	Crawler.prototype.crawl(function() {
 		console.log("Crawling complete.");
-		console.log(CheerioCrawler.static_assets);
+		console.log(Crawler.static_assets);
 	});
 }
 
-CheerioCrawler.prototype.crawl = function(callback) {
-	var next_page = CheerioCrawler.pages_to_visit.pop();
+Crawler.prototype.crawl = function(callback) {
+	var next_page = Crawler.pages_to_visit.pop();
 	if (next_page == undefined) {
 		console.log('next_page is undefined so stopping crawl.');
 		callback();
 	}
-	console.log('Crawl #' + ++CheerioCrawler.crawl_counter);
+	console.log('Crawl #' + ++Crawler.crawl_counter);
 	console.log(next_page);
-	if (next_page in CheerioCrawler.pages_visited) {
+	if (next_page in Crawler.pages_visited) {
 		// Already visited this page, so repeat the crawl
 		console.log(next_page + ' already visited - Repeating crawl...');
-		CheerioCrawler.prototype.crawl(callback);
+		Crawler.prototype.crawl(callback);
 	}
 	else {
-		CheerioCrawler.prototype.visit_page(next_page, function() {
+		Crawler.prototype.visit_page(next_page, function() {
 			console.log("Page " + next_page + " successfully visited.");
-			CheerioCrawler.prototype.crawl(callback);
+			Crawler.prototype.crawl(callback);
 		});	// New page we haven't visited
 	}
 }
 
-CheerioCrawler.prototype.visit_page = function(url, callback) {
-	CheerioCrawler.pages_visited[url] = true;	// Add visit page to the set
+Crawler.prototype.visit_page = function(url, callback) {
+	Crawler.pages_visited[url] = true;	// Add visit page to the set
 	console.log("Added " + url + " to pages_visited.");
 	console.log("Crawling page: " + url);
 	request(url, function (err, response, body) {	
@@ -61,8 +61,8 @@ CheerioCrawler.prototype.visit_page = function(url, callback) {
 	   if(response.statusCode === 200) {
 	     // Parse the DOM body
 	     var $ = cheerio.load(body);
-	     CheerioCrawler.prototype.collect_internal_links($);
-	     CheerioCrawler.prototype.scrape_static_assets(url, $, function() {
+	     Crawler.prototype.collect_internal_links($);
+	     Crawler.prototype.scrape_static_assets(url, $, function() {
 	     	callback();
 	     });
 	   }
@@ -73,7 +73,7 @@ CheerioCrawler.prototype.visit_page = function(url, callback) {
 	});
 }
 
-CheerioCrawler.prototype.collect_internal_links = function($) {
+Crawler.prototype.collect_internal_links = function($) {
 	var all_relative_links = [];
 	var all_absolute_links = [];
 
@@ -91,11 +91,11 @@ CheerioCrawler.prototype.collect_internal_links = function($) {
 	console.log("Found " + all_absolute_links.length + " absolute links");
 
 	relative_links.each(function() {
-		CheerioCrawler.pages_to_visit.push(CheerioCrawler.base_url + $(this).attr('href'))
+		Crawler.pages_to_visit.push(Crawler.base_url + $(this).attr('href'))
 	});
 }
 
-CheerioCrawler.prototype.scrape_static_assets = function(url, $, callback) {
+Crawler.prototype.scrape_static_assets = function(url, $, callback) {
 	var $head = $('head');
 	var $body = $('body');
 	var scraped_assets = [];
@@ -119,8 +119,8 @@ CheerioCrawler.prototype.scrape_static_assets = function(url, $, callback) {
 		url: url,
 		assets: scraped_assets
 	};
-	CheerioCrawler.static_assets.push(scraped_assets_JSON);
+	Crawler.static_assets.push(scraped_assets_JSON);
 	callback();
 }
 
-module.exports = CheerioCrawler;
+module.exports = Crawler;
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 var Crawler = require('../crawler.js');
-var CheerioCrawler = require('../cheerio-crawler.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -11,7 +10,7 @@ router.get('/', function(req, res, next) {
 /* POST url */
 router.post('/', function (req, res, next) {
 	var url = req.body.url;
-	var crawler = new CheerioCrawler(url);
+	var crawler = new Crawler(url);
 	crawler.start();
 });
 
